03a0bd993109a6ccf35e30575f298ce15c54c519
CassioMarte <135394169+CassioMarte@users.noreply.github.com>
2025-11-16 06:00:54 +0000

    feat: test

--- src/views/create_people_view.py
+++ src/views/create_people_view.py
@@ -1,5 +1,6 @@
 from src.controllers.interfaces.create_people_controller_interface import CreatePeopleControllerInterface
 from src.views.http_types.http_request import HttpRequest
+from src.validators.person_creator_validator import person_creator_validator
 from src.views.http_types.http_response import HttpResponse
 from .interface.view_interface import ViewInterface
 
@@ -8,6 +9,7 @@ class CreatePeopleView(ViewInterface):
         self.__create_people_controller = create_people_controller
 
     def handle(self, http_request: HttpRequest) -> HttpResponse:
+        person_creator_validator(http_request)
         person_info = http_request.body
         create_person = self.__create_people_controller.create(person_info)
 
